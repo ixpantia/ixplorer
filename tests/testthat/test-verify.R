5bed6aa7d43ea31874bf65f6759e7b30c470a4cc
ronny <ronny@ixpantia.com>
2018-12-13 08:11:44 -0600

    Ref #39 primeras pruebas

---
+++ tests/testthat/test-verify.R
@@ -0,0 +1,23 @@
+context("verificacion")
+
+test_that("Finds file .ixplorer", {
+  file <- verify_ixplorer_file()
+  expect_true(any(class(file) == "data.frame"))
+})
+
+
+test_that("Detects missing data PROJECT access value", {
+  file <- readr::read_csv("../testdata/.ixplorer") %>%
+    tidyr::separate(col = V1, into = c("object", "value"), sep = " ")
+  expect_true(verify_ixuser(file) == "There is no ixplorer USER, please use the Authentication gadget")
+})
+
+test_that("")
+
+# Hay un error o paso para verificar con posiciones de datos
+# Si eliminio una linea, la posicion varia y queda mal las condiciones
+# dentro de los verify.
+
+# el msj de set_authentication se esta repitiendo. YO habia provocado eso
+# adrede porque no hacia un print del msj y tuve que hacerlo explicito
+# ahora se esta comportando imprimiendo dos veces.
