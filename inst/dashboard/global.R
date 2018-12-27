55b2de7fb31435d4c66b2f66cf7e94b26f3ea94d
ronny <ronny@ixpantia.com>
2018-12-27 11:15:38 -0600

    Ref #1 intento por hacer validacion con gitear all dashboard

--- inst/dashboard/global.R
+++ inst/dashboard/global.R
@@ -4,16 +4,24 @@ library(plotly)
 library(lubridate)
 library(stringr)
 library(gitear)
+# library(ixplorer)
 
 # Set global options ----------------------------------------------------------
+## Si pongo estas funciones no sirve el dashboard. da un error
+access_file <- verify_ixplorer_file()
+# set_authentication(access_data = access_file)
 
 # Define global functions -----------------------------------------------------
 
 # example data ----------------------------------------------------------------------
 
   get_data <- function() {
+    proyectos <- gitear::get_list_repos_org(
+      base_url = Sys.getenv("IXURL"),
+      api_key = Sys.getenv("IXTOKEN"),
+      org = Sys.getenv("IXPROJECT"))
     lista_proyectos <- list()
-    lista_proyectos$ixplorer <- c("sitio_pruebas", "asignaciones")
+    lista_proyectos$ixplorer <- proyectos$name
     lista_proyectos$project_2 <- c("hola", "hello", "goodbye")
     return(lista_proyectos)
   }
