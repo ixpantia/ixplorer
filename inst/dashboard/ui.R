9f261022404dd6ff7af6620e78885b161fd6799c
ronny <ronny@ixpantia.com>
2019-01-10 16:11:43 -0600

    cambios en boton

--- inst/dashboard/ui.R
+++ inst/dashboard/ui.R
@@ -3,5 +3,5 @@
 ui <- navbarPage("ixplorer",
   id = "tabs",
   actionButton("close_app", label = "Close ixplorer Dashboard",
-               style = "color: #fff; background-color: #337ab7")
+               style = "color: #fff; background-color: #2B7C8D")
   )
