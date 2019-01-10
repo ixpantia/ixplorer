2f4cfe06529929c63bda125891919c0648e3c93c
ronny <ronny@ixpantia.com>
2019-01-10 16:25:33 -0600

    arreglo botones de los gadget

--- R/create_ticket.R
+++ R/create_ticket.R
@@ -12,13 +12,11 @@ NULL
 create_tickets <- function() {
 
   ui <- miniPage(
-    gadgetTitleBar("Create a new ticket",
+    miniTitleBar("Create a new ticket",
                    left = miniTitleBarCancelButton(inputId = "cancel",
                                                    label = "Cancel",
-                                                   primary = FALSE),
-                   right = miniTitleBarButton(inputId = "done",
-                                              label = "Done",
-                                              primary = TRUE)),
+                                                   primary = TRUE)
+                   ),
 
     miniContentPanel(
       verbatimTextOutput("warning", placeholder = FALSE),
@@ -55,17 +53,7 @@ create_tickets <- function() {
       return(msg)
     })
 
-    # ----------------------------------------------------------------
-    observeEvent(input$done, {
-      gitear::create_issue(base_url = Sys.getenv("IXURL"),
-                           api_key = Sys.getenv("IXTOKEN"),
-                           owner = Sys.getenv("IXPROJECT"),
-                           repo = Sys.getenv("IXREPO"),
-                           title = input$ticket_title,
-                           body =  input$ticket_description)
-      stopApp(NULL)
-    })
-
+    # Botones ----------------------------------------------------------------
     observeEvent(input$cancel, {
       # do nothing
       stopApp(NULL)
