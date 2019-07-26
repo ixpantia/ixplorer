0f446223c45435590588dee52e88251f13639974
ronny <ronny@ixpantia.com>
2019-07-26 09:12:20 -0600

    Ref #21 cambios en create ticket

--- R/create_ticket.R
+++ R/create_ticket.R
@@ -61,22 +61,36 @@ create_tickets <- function() {
     })
 
     observeEvent(input$create, {
-      tryCatch(
+      autentificacion <- tryCatch(
         {
-          gitear::create_issue(base_url = Sys.getenv("IXURL"),
-                               api_key = Sys.getenv("IXTOKEN"),
-                               owner = Sys.getenv("IXPROJECT"),
-                               repo = Sys.getenv("IXREPO"),
-                               title = input$ticket_title,
-                               body =  input$ticket_description)
+          gitear::create_issue(
+            base_url = Sys.getenv("IXURL"),
+            api_key = Sys.getenv("IXTOKEN"),
+            owner = Sys.getenv("IXPROJECT"),
+            repo = "prueba",
+            # repo = Sys.getenv("IXREPO"),
+            title = "prueba 2",
+            body = "prueba")
         },
-        error = function(e) {
-          print("No se ha creado ningún tiquete debido a credenciales inválidas. Porfavor use el gadget de autentificación.")
+        error = function(cond) {
+          autentificacion <- "error ronny"
         }
       )
+      tryCatch(1, finally = print("Hello"))
+
+
+
+          # title = input$ticket_title,
+          # body =  input$ticket_description)
+        },
+
+        }
+        # "No se ha creado ningún tiquete debido a credenciales inválidas. Porfavor use el gadget de autentificación."
+        )
       stopApp(NULL)
     })
   }
 
   runGadget(ui, server, viewer = dialogViewer("ixplorer"))
 }
+
