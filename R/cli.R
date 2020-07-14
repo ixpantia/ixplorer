30d505bc689e8e01b14c9eee64ffb4065f664eee
patrick <patrick@ixpantia.com>
2020-07-14 08:42:18 -0600

    Incluye lag en funciones para ver tiquetes

--- R/cli.R
+++ R/cli.R
@@ -5,10 +5,13 @@
 #' @param owner el nombre del proyecto donde se encuentra el repositorio en
 #' ixplorer
 #' @param repository el nombre del repositorio donde están los tiquetes
+#' @param lag es el tiempo en días que se quiere ver hacia atrás. Por ejemplo si
+#' se quiere ver los tiquetes creados en los últimos 7 días, lag = 7. Por
+#' defecto muestra todos los tiquetes sin ningún lag.
 #'
 #' @export
 list_open_tickets <- function(instance, owner, repository = "current",
-                              lag = 7) {
+                              lag = NULL) {
 
   credenciales <- tryCatch(
     keyring::key_get(paste0("token_", instance)),
@@ -41,6 +44,25 @@ list_open_tickets <- function(instance, owner, repository = "current",
       repo = repository)
 
     list <- list %>%
+      mutate(created_at = lubridate::ymd_hms(created_at) -
+               lubridate::hours(6),
+             updated_at = lubridate::ymd_hms(updated_at) -
+               lubridate::hours(6),
+             closed_at = lubridate::ymd_hms(closed_at) -
+               lubridate::hours(6),
+             user.last_login = lubridate::ymd_hms(user.last_login) -
+               lubridate::hours(6),
+             user.created = lubridate::ymd_hms(user.created) -
+               lubridate::hours(6),
+             milestone.due_on = lubridate::ymd_hms(milestone.due_on) -
+               lubridate::hours(6),
+             assignee.last_login = lubridate::ymd_hms(assignee.last_login) -
+               lubridate::hours(6),
+             assignee.created = lubridate::ymd_hms(assignee.created) -
+               lubridate::hours(6)) %>% #Deja hora Costa Rica
+      {if (!is.null(lag)) {
+        dplyr::filter(., created_at >= Sys.Date() - lubridate::days(lag))
+      } else {.}} %>%
       dplyr::select(number, title, milestone.title) %>%
       dplyr::arrange(milestone.title, number) %>%
       dplyr::rename(nr = number,
@@ -58,10 +80,13 @@ list_open_tickets <- function(instance, owner, repository = "current",
 #' @param owner el nombre del proyecto donde se encuentra el repositorio en
 #' ixplorer
 #' @param repository el nombre del repositorio donde están los tiquetes
+#' @param lag es el tiempo en días que se quiere ver hacia atrás. Por ejemplo si
+#' se quiere ver los tiquetes cerrados en los últimos 7 días, lag = 7. Por
+#' defecto muestra todos los tiquetes sin ningún lag.
 #'
 #' @export
 list_closed_tickets <- function(instance, owner, repository = "current",
-                                lag = 7) {
+                                lag = NULL) {
 
   credenciales <- tryCatch(
     keyring::key_get(paste0("token_", instance)),
@@ -87,12 +112,31 @@ list_closed_tickets <- function(instance, owner, repository = "current",
   }
 
   list <-  gitear::get_issues_closed_state(
-    base_url = paste0("https://", credenciales$instancia),
+    base_url = paste0("https://", credenciales$url),
     api_key = credenciales$token,
-    owner = owner_repository,
+    owner = owner,
     repo = repository)
 
   list <- list %>%
+    mutate(created_at = lubridate::ymd_hms(created_at) -
+             lubridate::hours(6),
+           updated_at = lubridate::ymd_hms(updated_at) -
+             lubridate::hours(6),
+           closed_at = lubridate::ymd_hms(closed_at) -
+             lubridate::hours(6),
+           user.last_login = lubridate::ymd_hms(user.last_login) -
+             lubridate::hours(6),
+           user.created = lubridate::ymd_hms(user.created) -
+             lubridate::hours(6),
+           milestone.due_on = lubridate::ymd_hms(milestone.due_on) -
+             lubridate::hours(6),
+           assignee.last_login = lubridate::ymd_hms(assignee.last_login) -
+             lubridate::hours(6),
+           assignee.created = lubridate::ymd_hms(assignee.created) -
+             lubridate::hours(6)) %>% #Deja hora Costa Rica
+    {if (!is.null(lag)) {
+      dplyr::filter(., closed_at >= Sys.Date() - lubridate::days(lag))
+    } else {.}} %>%
     dplyr::select(number, title, milestone.title) %>%
     dplyr::arrange(milestone.title, number) %>%
     dplyr::rename(nr = number,
