a2e38d02d42ec0836d0c1d43a0223ef9b0889048
FvD <frans@ixpantia.com>
2021-12-18 14:40:03 -0600

    Closes #14

--- R/git.R
+++ R/git.R
@@ -1,10 +1,10 @@
 #' @title Add the upstream to the repository that is active in RStudio.
-#' @description Add the upstream to the repository that is active in RStudio
-#' at the moment
+#' @description Add the upstream to the repository that is active in RStudio at
+#'   the moment
 #'
 #' @param instance ixplorer instance (Eg: "secure", "masterclass", "prueba")
 #' @param owner the name of the project where the repository is located in
-#' ixplorer
+#'   ixplorer
 #'
 #' @export
 add_upstream <- function(instance, owner) {
@@ -42,4 +42,35 @@ add_upstream <- function(instance, owner) {
     ".git")
 
   git2r::remote_add(repo = camino, name = "upstream", url = proyecto_madre)
+}
+
+
+
+#' @title Extend git credential cache time-out
+#' @description Set the number of seconds before the git credential cache times
+#'   out. Note that this is written for work on remote servers where we do not
+#'   want to store our git credentials (other might have acces to it, or
+#'   impersonate us). On a trusted machine the most common setting would be ( on
+#'   a linux machine) <git credential.helper store>. However, some caching is
+#'   required when working on a remote server because frequent commit-push
+#'   cycles where you have to write out your username and password every time is
+#'   not nice.
+#'
+#'   By default this will only apply to the repo you are working in. If you call
+#'   the global version take care that this is what you want. Also by default
+#'   the timeout is set to 4 hours (14400 seconds).
+#'
+#' @param timeout number of seconds before timeout
+#' @param global whether the change should be global or local to the repo
+#'
+#' @export
+set_git_timeout <- function(timeout = 14400, global = FALSE) {
+
+  if (global == TRUE) {
+  instruction <- paste0("git config --global credential.helper cache --timeout=", timeout)
+  } else {
+  instruction <- paste0("git config credential.helper cache --timeout=", timeout)
+  }
+
+  system(instruction)
 }
