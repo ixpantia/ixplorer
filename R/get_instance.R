21af5456d8be3d8156a622a5ca05f5cef807a7dc
Daniel Granados <granadoscampos.daniel@gmail.com>
2022-02-18 14:17:14 -0600

    Ref #78. Adds function to get instance easily

---
+++ R/get_instance.R
@@ -0,0 +1,41 @@
+#' Get the active instance in the session
+#'
+#' @description By default, functions that get tickets and pull requests
+#' use the active instance in the session. This functions gets this
+#' active instance.
+#'
+#' @return a character of the active issue in the session
+#' @export
+#'
+#' @examples
+get_instance <- function() {
+
+
+  # Look in session
+  if (Sys.getenv("ixplorer_instance") != "") {
+
+    instance <- Sys.getenv("ixplorer_instance")
+
+    # No active instance in session
+  } else if (Sys.getenv("ixplorer_instance") == "") {
+
+    saved_instances <- keyring::keyring_list() %>%
+      filter(stringr::str_detect(keyring, "ixplorer_"))
+
+    # Choose last saved instance
+    if (nrow(saved_instances) > 0) {
+
+      last_saved <- saved_instances[1,1]
+      instance <- last_saved
+
+      # No instance found
+    } else {
+
+      instance <- "none"
+
+      }
+    }
+
+  return(instance )
+
+}
