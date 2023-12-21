00bccb884c96a996750869928f4bacf28843f5c3
olger-ixpantia <olger@ixpantia.com>
2023-12-21 03:49:37 -0600

    Resolved merge conflicts

--- R/publish_wiki.R
+++ R/publish_wiki.R
@@ -29,7 +29,7 @@
 #'  push set in TRUE, if you like to do manually, set FALSE. Default is TRUE.
 #' @param quiet_render_logs if you like to see the render logs of rmarkdown,
 #'  set in TRUE, in otherwise set FALSE. Default is TRUE.
-#' @param new_file_name Optional custom name for the published file.
+#' @param new_file_name the new name for the markdown file.
 #' @return Publish a md into wiki repository
 #' @examples
 #'
